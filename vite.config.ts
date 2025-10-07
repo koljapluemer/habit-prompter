2ad80282f8fd3dd2e3c2c622506f912453e2d797
Kolja Sam <github@koljapluemer.com>
2025-10-07 15:08:25 +0200

    setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
